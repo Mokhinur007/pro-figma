1c4181a557853087c558a18871701eb121c2a5c4
Mokhinur007 <you@example.com>
2024-11-13 22:30:30 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Home from './pages/home';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Routes>
+    <Route path="/home" element={<Home />} />
+    </Routes>
     </div>
   );
 }
